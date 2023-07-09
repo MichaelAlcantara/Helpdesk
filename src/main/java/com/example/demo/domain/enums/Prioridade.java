569824e0685be1c350ecb7f96d65463445c44d74
MichaelAlcantara <michael.raposo@hotmail.com>
2023-07-09 13:21:40 -0300

    tipos enumerados criados

---
+++ src/main/java/com/example/demo/domain/enums/Prioridade.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain.enums;
+
+public enum Prioridade {
+
+    BAIXO(0, "BAIXO"), MEDIO(1, "MEDIO"), ALTO(2, "ALTO");
+
+    private Integer codigo;
+    private String descricao;
+
+    private Prioridade(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Prioridade toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+        for (Prioridade x : Prioridade.values()) {
+            if (cod.equals(x.getCodigo())) {
+                return x;
+            }
+        }
+        
+        throw new IllegalArgumentException("Prioridade inválida!");
+    }
+}
