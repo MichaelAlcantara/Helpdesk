77015756bd1b611b617037911edb5f47b2745803
MichaelAlcantara <michael.raposo@hotmail.com>
2023-11-12 15:29:40 -0300

    Exceção para CPF e E-mail já cadastrado

--- src/main/java/com/michael/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/michael/helpdesk/repositories/PessoaRepository.java
@@ -3,7 +3,12 @@ package com.michael.helpdesk.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.michael.helpdesk.domain.Pessoa;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
