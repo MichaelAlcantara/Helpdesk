2f6b39f1decb4525d18986da07164957b01e3ea5
MichaelAlcantara <michael.raposo@hotmail.com>
2023-11-12 14:05:38 -0300

    Padrão DTO - Tecnico

--- src/main/java/com/michael/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/michael/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.michael.helpdesk.domain.Tecnico;
+import com.michael.helpdesk.domain.dtos.TecnicoDTO;
 import com.michael.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,10 +19,10 @@ public class TecnicoResource {
 	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		
 		Tecnico obj = this.service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 		
 	}
 }
