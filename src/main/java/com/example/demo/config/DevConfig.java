2328e8085913d55a0f77dc04abc816064d487ae8
MichaelAlcantara <michael.raposo@hotmail.com>
2023-11-12 12:35:21 -0300

    Perfil de Desenvolvimento

---
+++ src/main/java/com/example/demo/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.demo.services.DBService;
+
+//uma Classe de configuração
+@Configuration
+//Perfil de teste
+@Profile("dev")
+public class DevConfig {
+    
+    @Autowired
+    private DBService dbService;
+    
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+    
+    //Assim que roda esse perfil, o que esta dentro desse método irá subir
+    @Bean
+    public boolean instanciaDB() {
+        if(value.equals("create")) {
+        	this.dbService.instanciaDB();
+        }
+        return false;
+
+    }
+}
