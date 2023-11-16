83eda2c3c6f838c59ed2cc07c72a8fd93f89f8a6
MichaelAlcantara <michael.raposo@hotmail.com>
2023-11-16 17:50:32 -0300

    Autorização

--- src/main/java/com/michael/helpdesk/security/JWTAuthenticationFilter.java
+++ src/main/java/com/michael/helpdesk/security/JWTAuthenticationFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
