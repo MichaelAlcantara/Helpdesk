5a9e75930d3f0687e5d679e79c2bf681ada97a7e
MichaelAlcantara <michael.raposo@hotmail.com>
2023-11-17 12:09:16 -0300

    Melhorias no HTML e CSS do componente Tecnico-list

--- helpdesk-frontend/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ helpdesk-frontend/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id : 1,
+      nome : 'Michael',
+      cpf : '12345678910',
+      email : "michael@email.com",
+      senha : '123',
+      perfis : ['0'],
+      dataCriacao : '17/11/2023'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', "acoes"];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
