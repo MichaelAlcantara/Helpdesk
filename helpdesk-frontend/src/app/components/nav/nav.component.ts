a4b1ee46f154066cc2c0ab320a4001d34a4abee1
michael <michael.raposo@hotmail.com>
2023-11-26 13:22:45 -0300

    HTML e CSS para o componente Tecnico-create

--- helpdesk-frontend/src/app/components/nav/nav.component.ts
+++ helpdesk-frontend/src/app/components/nav/nav.component.ts
@@ -21,7 +21,7 @@ export class NavComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['home']);
+    this.router.navigate(['tecnicos/create']);
   }
 
   logout(){
