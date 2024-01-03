f5b1f5bca32dc2ecdacd964b64a66650aee97bf0
michael <michael.raposo@hotmail.com>
2024-01-03 17:58:24 -0300

    Criação de chamados

--- helpdesk-backend/src/main/java/com/michael/helpdesk/services/DBService.java
+++ helpdesk-backend/src/main/java/com/michael/helpdesk/services/DBService.java
@@ -41,13 +41,26 @@ public class DBService {
         
         Tecnico tec2 = new Tecnico(null, "Fulano", "44629709002", encoder.encode("123"), "Fulano@email.com");
         tec2.addPerfil(Perfil.TECNICO);
+
+        Tecnico tec3 = new Tecnico(null, "Albert", "96921976033", encoder.encode("123"), "Albert@email.com");
+        tec2.addPerfil(Perfil.TECNICO);
+
+        Tecnico tec4 = new Tecnico(null, "Franciso", "47709983090", encoder.encode("123"), "Franciso@email.com");
+        tec2.addPerfil(Perfil.ADMIN);
+        
+        Tecnico tec5 = new Tecnico(null, "Juliano", "15537387050", encoder.encode("123"), "Juliano@email.com");
+        tec2.addPerfil(Perfil.TECNICO);
         
         Cliente cli1 = new Cliente(null, "Rodolfo", "65305081041", encoder.encode("123"), "rodolfo@email.com");
         
+        Cliente cli2 = new Cliente(null, "Enzo", "37307969092", encoder.encode("123"), "Enzo@email.com");
+        
+        Cliente cli3 = new Cliente(null, "Maria", "91346686084", encoder.encode("123"), "Maria@email.com");
+        
         Chamado c1 = new Chamado(null, Prioridade.MEDIO, Status.ANDAMENTO, "Chamado 1", "Primeiro chamado", tec1, cli1);
         
-        tecnicoRepository.saveAll(Arrays.asList(tec1, tec2));
-        clienteRepository.saveAll(Arrays.asList(cli1));
+        tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5));
+        clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
         chamadoRepository.saveAll(Arrays.asList(c1));
 
     }
